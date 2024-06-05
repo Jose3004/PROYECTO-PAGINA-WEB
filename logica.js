11343f2ad22bd1d9b4db7cab523674d5922ea3b9
Jose3004 <119958493+Jose3004@users.noreply.github.com git config --global user.email "119958493+Jose3004@users.noreply.github.com>
2024-06-04 22:14:50 -0500

    Configuración inicial proyecto

---
+++ logica.js
@@ -0,0 +1,42 @@
+function logica() {
+
+    var numeros = ["Christian" , "David" , "Carlos" , "Andres" ]
+
+
+    //Ciclos
+    /*
+      Christian i = 0; 0 < 5 ; i++ // 0 = 0 + 1
+      David i = 0; 0 < 5 ; i++ // 0 = 0 + 1
+      Carlos i = 0; 0 < 5 ; i++ // 0 = 0 + 1
+      Miguel i = 0; 0 < 5 ; i++ // 0 = 0 + 1
+      Andres i = 0; 0 < 5 ; i++ // 0 = 0 + 1
+      Termina Aqui i = 0; 0 < 5 ; i++ // 0 = 0 + 1
+    */
+
+    for (let i = 0; i < numeros.length; i++) {
+        console.log(numeros[i]) 
+    }
+
+
+
+
+
+
+
+
+
+}
+
+//var = Global y Local // No se reinician - No libera espacio en memoria
+//let = Local          // Se reinician    - Libera memoria
+
+const servidor = "10.5.5.2.1";
+var prueba2 = 0;
+
+
+function name (prueba) {
+    var prueba2 = 0 
+}
+
+
+
